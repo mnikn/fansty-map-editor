6eadedd13c0bab9a3d52c8273d11ec954c8ad972
mnikn <329121948@qq.com>
2021-02-21 11:46:00 +0800

    feat: update

--- addons/fantasy_map_editor/EditorMapContainer.gd
+++ addons/fantasy_map_editor/EditorMapContainer.gd
@@ -5,12 +5,13 @@ onready var Map = $ViewportContainer/Viewport/Map
 onready var Preview = $Preview/Viewport/Container
 
 func _process(delta):
+    self.Preview.set("custom_styles/panel", self.Map.NodeContainer.get("custom_styles/bg"))
     for child in self.Preview.get_children():
         self.Preview.remove_child(child)
     for node in self.Map.NodeContainer.get_children():
         if node is GraphNode:
             var clone_node = node.duplicate()
             clone_node.shape_points = node.shape_points
-            clone_node.rect_position = node.rect_position
+            clone_node.rect_position = node.offset
             clone_node.selected = false
             self.Preview.add_child(clone_node)
